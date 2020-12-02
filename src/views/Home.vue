a5486cccf9b65c7e70f3f5917a1697f132505ea5
QIUWEI-hu <2498257693@qq.com>
2020-12-02 17:03:09 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,13 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+export default {};
 </script>
+<style lang="less" scoped>
+.home {
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+  border: 1px solid #000;
+}
+</style>
